9d699ffb9c7c03e0d8b70e0b82a913a87479c91c
cosmic-zip <36008397+prism-zip@users.noreply.github.com>
2024-07-15 10:48:48 -0300

    update

--- bin/panzer_maid.dart
+++ bin/panzer_maid.dart
@@ -1,5 +1,10 @@
-import 'package:panzer_maid/panzer_maid.dart' as panzer_maid;
+import 'package:panzer_maid/tinybox/utils.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${panzer_maid.calculate()}!');
+  var log = logger("test", "in", "out");
+  var mog = puts("Hello puts!", color: "black");
+  Map<String, dynamic> db = importBank();
+  print(db["generic"]);
+  print(arguments);
+  queryMaker(arguments);
 }
