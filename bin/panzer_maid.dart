a05a5720d24c2c2feb7869e65571a1e0f0d084a8
cosmic-zip <36008397+prism-zip@users.noreply.github.com>
2024-07-15 11:19:54 -0300

    repo cleanup

--- bin/panzer_maid.dart
+++ bin/panzer_maid.dart
@@ -1,12 +1,1 @@
-import 'package:panzer_maid/tinybox/utils.dart';
-import 'package:panzer_maid/tinybox/panzer_status.dart';
-
-void main(List<String> arguments) {
-  var log = logger("test", "in", "out");
-  var mog = puts("Hello puts!", color: "black");
-  Map<String, dynamic> db = importBank();
-  print(db["generic"]);
-  print(arguments);
-  queryMaker(arguments);
-  panzerStatus();
-}
+void main(List<String> arguments) {}
---
+++ lib/domains/web/attack.dart
@@ -0,0 +1,5 @@
+void domain_dns_lookup_sub_domin() {}
+void domain_dns_lookup_dirs() {}
+void domain_dns_lookup_whois() {}
+void domain_services_scanner() {}
+void domain_services_attack() {}
