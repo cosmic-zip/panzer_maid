8a6b1aed73c07ba6c36f14a82c0c4d43f3adf7e2
cosmic-zip <36008397+prism-zip@users.noreply.github.com>
2024-07-19 15:17:17 -0300

    change manual module name

--- bin/panzer_maid.dart
+++ bin/panzer_maid.dart
@@ -1,9 +1,7 @@
-import 'package:panzer_maid/tinybox/panzer_shell_utils.dart';
 import 'package:panzer_maid/tinybox/panzer_utils.dart';
 import 'package:panzer_maid/tinybox/panzer_tui.dart';
 
 void main(List<String> arguments) async {
   panzerMaidWelcome();
   panzerRunner(arguments);
-  userManual(module: 'all');
 }
