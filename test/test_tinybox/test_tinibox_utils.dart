502d54fea8892523b4b7043f353cf6b0b45f1707
cosmic-zip <36008397+prism-zip@users.noreply.github.com>
2024-07-20 07:19:32 -0300

    add basic tests

---
+++ test/test_tinybox/test_tinibox_utils.dart
@@ -0,0 +1,58 @@
+import 'package:panzer_maid/tinybox/panzer_utils.dart';
+import 'package:test/test.dart';
+
+void test_tinibox_utils() {
+  group('logger', () {
+    test('Test logger function', () {
+      var log = logger("name", "origin", "output");
+      expect(log, equals(true));
+    });
+  });
+
+  group('Puts', () {
+    test('Test pust func with an empty string', () {
+      var out = puts("");
+      expect(out, "");
+    });
+    test('Test pust func with an random string (uses bold output by default)',
+        () {
+      var out = puts("random");
+      expect(out, "\x1B[1mrandom\x1B[0m");
+    });
+    test('Test pust func with an random string with color magenta', () {
+      var out = puts("random", color: 'magenta');
+      expect(out, "\x1B[1m\x1B[35mrandom\x1B[0m");
+    });
+  });
+
+  group('importBank', () {
+    test('Import db.json and test its contents', () async {
+      var db = importBank();
+
+      var mocked_out = {
+        "description":
+            "Securely deletes and overwrites the contents of a device seven times",
+        "name": "nuke.hd",
+        "command": "shred -vzn 7 @@device"
+      };
+
+      expect(await db['general'][0], equals(mocked_out));
+    });
+    test('Import db.json and search for invalid fields', () async {
+      var db = importBank();
+
+      expect(await db['REEEE'], equals(null));
+    });
+  });
+
+  group('flawlessExec', () {
+    test('prints reminder and exits with 255 for empty arguments', () async {
+      var test_string = [];
+      expect(await flawlessExec(test_string), equals(255));
+    });
+    test('Executes ls -lha and returns 0', () async {
+      var test_string = ["--raw", "ls -la"];
+      expect(await flawlessExec(test_string), equals(0));
+    });
+  });
+}
