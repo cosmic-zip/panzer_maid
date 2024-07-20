6da6c50d05b0a47b314c4a12a44f237ddf9197c5
cosmic-zip <36008397+prism-zip@users.noreply.github.com>
2024-07-20 14:27:33 -0300

    small fix

--- lib/tinybox/panzer_shell.dart
+++ lib/tinybox/panzer_shell.dart
@@ -1,16 +1,28 @@
 import 'package:panzer_maid/tinybox/panzer_manual.dart';
+import 'package:panzer_maid/tinybox/panzer_status.dart';
 import 'package:panzer_maid/tinybox/panzer_tui.dart';
 import 'package:panzer_maid/tinybox/panzer_utils.dart';
 
 Future<int> panzerMaidShell(List<String> terminalArgs) async {
-  if (terminalArgs.isEmpty) return 255;
+  if (terminalArgs.isEmpty) {
+    panzerMaid();
+
+    return 255;
+  }
+  ;
   var option = terminalArgs[0];
   switch (option) {
     case '--help' || '-h':
       userManual(terminalArgs);
       return 0;
-    case '--welcome' || '-w':
+    case '--well' || '-w':
       panzerMaidWelcome();
+      return 0;
+    case '--panzer':
+      panzerMaid();
+      return 0;
+    case '--status':
+      panzerStatus();
       return 0;
     case '--raw':
       flawlessExec(terminalArgs);
