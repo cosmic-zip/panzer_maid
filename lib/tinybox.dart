a3edb3ede64589d872441dd63ece74670192d5f5
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-07-22 22:03:39 -0300

    update unix uitls

---
+++ lib/tinybox.dart
@@ -0,0 +1,24 @@
+import 'package:panzer_maid/tinybox/shell.dart';
+import 'package:panzer_maid/tinybox/utils.dart';
+import 'package:panzer_maid/unix/unixutils.dart';
+
+class TinyBox {
+  List<String> terminalArgs = [];
+
+  TinyBox();
+
+  Future<int> exec() async {
+    if (terminalArgs.isEmpty) stdint('fail');
+    return panzerMaidShell(this.terminalArgs);
+  }
+
+  Future<int> flawless() {
+    if (terminalArgs.isEmpty) stdint('fail');
+    return flawlessExec(this.terminalArgs);
+  }
+
+  Future<int> tinybox() {
+    if (terminalArgs.isEmpty) stdint('fail');
+    return tinyBoxExec(this.terminalArgs, this.terminalArgs[0]);
+  }
+}
